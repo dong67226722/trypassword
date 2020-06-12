27271b4e6464369d2f5cbb0c56552cfef381465c
dong67226722 <huafa.dong@groupfu.com>
2020-06-12 23:01:01 +0800

    delete 0 show

--- trypassword.py
+++ trypassword.py
@@ -1,11 +1,14 @@
 password = 'a123456'
 i = 3
 while i > 0:
+	i = i - 1
 	pwd = input('请输入密码：')
 	if pwd == password:
 		print('登录成功！')
 		break
 	else:
-		i = i - 1
-		print('你还有', i ,'次机会')
-		
+		print('密码错误！')
+		if i > 0:
+			print('你还有', i ,'次机会')
+		else:
+			print('没机会了，账号已锁，请联络管理员！')
